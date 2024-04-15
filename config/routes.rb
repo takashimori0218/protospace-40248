31e56fd04317b2337f3637afbfa29dad8e7b5434
takashimori0218 <157961621+takashimori0218@users.noreply.github.com>
2024-04-15 19:55:25 +0900

    ユーザーデータ保存できた

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  post '/users/sign_up', to: 'devise/registrations#create'
   get 'prototypes/index'
   root to: "prototypes#index"
   resources :users, only: [:index, :new]
