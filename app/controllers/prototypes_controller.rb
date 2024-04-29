555a48fdaf2617fcd88181b4a72a72b62fbb635e
takashimori0218 <157961621+takashimori0218@users.noreply.github.com>
2024-04-29 21:07:34 +0900

    詳細ページまで完了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,28 @@
 class PrototypesController < ApplicationController
   def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+  
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   post '/users/sign_up', to: 'devise/registrations#create'
-  get 'prototypes/index'
   root to: "prototypes#index"
   resources :users, only: [:index, :new]
+
+  resources :prototypes, only: [:new, :create, :show, :edit] do
+    resources :comments, only: [:create]
+  end
+
   devise_scope :user do
-  get '/users/sign_out' => 'devise/sessions#destroy'
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 end
